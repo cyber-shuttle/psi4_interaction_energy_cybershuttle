528431aa4c4d9e23af4b9877c149889a3b11d694
AMW Arch <awallace43@gatech.edu>
2025-04-28 17:16:56 -0400

    transfer learning example

--- demo.py
+++ demo.py
@@ -1,18 +1,19 @@
-
 import psi4
-from qcportal import PortalClient
 from pprint import pprint as pp
-from qcelemental.models import Molecule
-import qcelemental as qcel
-from qcportal.singlepoint import SinglepointDataset, SinglepointDatasetEntry, QCSpecification
 import pandas as pd
 import numpy as np
 import re
 from qm_tools_aw import tools
-
-# need manybodydataset
-from qcportal.manybody import ManybodyDataset, ManybodyDatasetEntry, ManybodyDatasetSpecification, ManybodySpecification
 from pprint import pprint as pp
+# QCElemental Imports
+from qcelemental.models import Molecule
+import qcelemental as qcel
+# Dataset Imports
+from qcportal import PortalClient
+from qcportal.singlepoint import SinglepointDataset, SinglepointDatasetEntry, QCSpecification
+from qcportal.manybody import ManybodyDataset, ManybodyDatasetEntry, ManybodyDatasetSpecification, ManybodySpecification
+
+h2kcalmol = qcel.constants.hartree2kcalmol
 
 # |%%--%%| <sb2BSlStsm|BVc6W6uOta>
 
@@ -61,24 +62,19 @@ setup_qcarchive_qcfractal(
 
 !qcfractal-server --config=`pwd`/qcfractal/qcfractal_config.yaml start > qcfractal_server.log & disown
 
-# NOTE kill server when finished by running:
-#     ps aux | grep qcfractal-server | awk '{ print $2 }'
-#     kill -9 <PID>
-
 # |%%--%%| <CwEhpqwLXX|3HjtiyIuFg>
 
 !qcfractal-compute-manager --config=`pwd`/qcfractal/resources.yml > qcfractal_compute.log & disown
-# NOTE kill server when finished by running:;
-#    ps aux | grep qcfractal-server | awk '{ print $2 }'
-#    kill -9 <PID>
 
-# |%%--%%| <3HjtiyIuFg|hMCmRgdGJ4>
+#|%%--%%| <3HjtiyIuFg|2gmdKuBAuk>
+
+# NOTE kill server when finished by removing the # and executing:
+# !ps aux | grep qcfractal | awk '{ print $2 }' | xargs kill -9
+
+# |%%--%%| <2gmdKuBAuk|hMCmRgdGJ4>
 
 # Running a single job
 client = PortalClient("http://localhost:7777", verify=False)
-# for rec in client.query_records():
-#     pp(rec)
-
 mol = Molecule.from_data(
     """
      0 1
@@ -111,28 +107,50 @@ client.add_singlepoints(
     tag="local",
 )
 
+# Can print records
 # for rec in client.query_records():
 #     pp(rec.dict)
 #     pp(rec.error)
 
 # |%%--%%| <hMCmRgdGJ4|Z0wXrcgRq8>
 
-# Now create S22 Interaction Energy Dataset
-from s22 import geoms, ref_IEs
+# Creating a QCArchive Dataset...
+# Load in a dataset from a recent Sherrill work (Levels of SAPT II)
+df_LoS = pd.read_pickle("./combined_df_subset_358.pkl")
+print(df_LoS[['Benchmark', 'SAPT2+3(CCD)DMP2 TOTAL ENERGY aqz', 'MP2 IE atz', 'SAPT0 TOTAL ENERGY adz' ]])
 
-assert len(geoms) == len(ref_IEs), "Number of geometries and reference interaction energies do not match"
+# Limit to 100 molecules with maximum of 16 atoms to keep computational cost down
+df_LoS['size'] = df_LoS['atomic_numbers'].apply(lambda x: len(x))
+df_LoS = df_LoS[df_LoS['size'] <= 16]
+df_LoS = df_LoS.sample(200, random_state=42, axis=0).copy()
+df_LoS.reset_index(drop=True, inplace=True)
+print(df_LoS['size'].describe())
 
-# geoms is a list of qcelemental Molecule objects that can be used to create a
-# QCArchive dataset
-print(len(geoms), geoms)
+# Create QCElemntal Molecules to generate the dataset
+def qcel_mols(row):
+    """
+    Convert the row to a qcel molecule
+    """
+    atomic_numbers = [row['atomic_numbers'][row['monAs']], row['atomic_numbers'][row['monBs']]]
+    coords = [row['coordinates'][row['monAs']], row['coordinates'][row['monBs']]]
+    cm = [
+        [row['monA_charge'], row['monA_multiplicity']],
+        [row['monB_charge'], row['monB_multiplicity']],
+     ]
+    return tools.convert_pos_carts_to_mol(atomic_numbers, coords, cm)
+df_LoS['qcel_molecule'] = df_LoS.apply(qcel_mols, axis=1)
+geoms = df_LoS['qcel_molecule'].tolist()
+ref_IEs = df_LoS['Benchmark'].tolist()
+sapt0_adz = (df_LoS['SAPT0 TOTAL ENERGY adz'] * h2kcalmol).tolist()
 
 # |%%--%%| <Z0wXrcgRq8|i8ICwzPWaD>
 
 # Create client dataset
 
-ds_name = 'S22-multipoles'
-
-try:
+ds_name = 'S22-singlepoint'
+client_datasets = [i['dataset_name'] for i in client.list_datasets()]
+# Check if dataset already exists, if not create a new one
+if ds_name not in client_datasets:
     ds = client.add_dataset("singlepoint", ds_name,
                             f"Dataset to contain {ds_name}")
     print(f"Added {ds_name} as dataset")
@@ -148,41 +166,47 @@ try:
         entry_list.append(ent)
     ds.add_entries(entry_list)
     print(f"Added {len(entry_list)} molecules to dataset")
-except Exception:
+else:
     ds = client.get_dataset("singlepoint", ds_name)
     print(f"Found {ds_name} dataset, using this instead")
-    print(ds)
 
-# |%%--%%| <i8ICwzPWaD|dSW1A9HxYB>
+print(ds)
 
-# Set the method and basis for lower requirements?
-method, basis = "hf", "sto-3g"
+#|%%--%%| <i8ICwzPWaD|7ZAOPlzuUX>
 
-# Set the QCSpecification (QM interaction energy in our case)
-spec = QCSpecification(
-    program="psi4",
-    driver="energy",
-    method=method,
-    basis=basis,
-    keywords={
-        "d_convergence": 8,
-        "dft_radial_points": 99,
-        "dft_spherical_points": 590,
-        "e_convergence": 10,
-        "guess": "sad",
-        "mbis_d_convergence": 9,
-        "mbis_radial_points": 99,
-        "mbis_spherical_points": 590,
-        "scf_properties": ["mbis_charges", "MBIS_VOLUME_RATIOS"],
-        "scf_type": "df",
-    },
-    protocols={"wavefunction": "orbitals_and_eigenvalues"},
-)
-ds.add_specification(name=f"psi4/{method}/{basis}", specification=spec)
+# Can delete the dataset if you want to start over. Need to know dataset_id
+# client.delete_dataset(dataset_id=ds.id, delete_records=True)
+
+# |%%--%%| <7ZAOPlzuUX|dSW1A9HxYB>
+
+# Multipole Example
+# method, basis = "hf", "sto-3g"
+#
+# # Set the QCSpecification (QM interaction energy in our case)
+# spec = QCSpecification(
+#     program="psi4",
+#     driver="energy",
+#     method=method,
+#     basis=basis,
+#     keywords={
+#         "d_convergence": 8,
+#         "dft_radial_points": 99,
+#         "dft_spherical_points": 590,
+#         "e_convergence": 10,
+#         "guess": "sad",
+#         "mbis_d_convergence": 9,
+#         "mbis_radial_points": 99,
+#         "mbis_spherical_points": 590,
+#         "scf_properties": ["mbis_charges", "MBIS_VOLUME_RATIOS"],
+#         "scf_type": "df",
+#     },
+#     protocols={"wavefunction": "orbitals_and_eigenvalues"},
+# )
+# ds.add_specification(name=f"psi4/{method}/{basis}", specification=spec)
 
 # |%%--%%| <dSW1A9HxYB|vMkm00fo00>
 
-# Set the method and basis for lower requirements?
+# SAPT0 Example
 method, basis = "SAPT0", "cc-pvdz"
 
 # Set the QCSpecification (QM interaction energy in our case)
@@ -211,62 +235,29 @@ ds.status()
 # |%%--%%| <2JMCNlehez|g31JlHrgso>
 
 # Create client dataset
-
 ds_name_mb = 'S22-manybody'
-
-try:
+client_datasets = [i['dataset_name'] for i in client.list_datasets()]
+# Check if dataset already exists, if not create a new one
+if ds_name_mb not in client_datasets:
+    print("Setting up new dataset:", ds_name_mb)
     ds_mb = client.add_dataset("manybody", ds_name_mb,
                             f"Dataset to contain {ds_name_mb}")
     print(f"Added {ds_name_mb} as dataset")
-except Exception:
+    # Insert entries into dataset
+    entry_list = []
+    for idx, mol in enumerate(geoms):
+        ent = ManybodyDatasetEntry(name=f"S22-IE-{idx}", initial_molecule=mol)
+        entry_list.append(ent)
+    ds_mb.add_entries(entry_list)
+    print(f"Added {len(entry_list)} molecules to dataset")
+else:
     ds_mb = client.get_dataset("manybody", ds_name_mb)
     print(f"Found {ds_name_mb} dataset, using this instead")
-    print(ds)
 
-# Insert entries into dataset
+print(ds_mb)
 
-entry_list = []
-for idx, mol in enumerate(geoms):
-    print(mol)
-    ent = ManybodyDatasetEntry(name=f"S22-IE-{idx}", initial_molecule=mol)
-    entry_list.append(ent)
-ds_mb.add_entries(entry_list)
-print(f"Added {len(entry_list)} molecules to dataset")
-
-# Set the method and basis for lower requirements?
-method, basis = "hf", "sto-3g"
-
-# Set the QCSpecification (QM interaction energy in our case)
-
-qc_spec_mb = QCSpecification(
-    program="psi4",
-    driver="energy",
-    method=method,
-    basis=basis,
-    keywords={
-        "d_convergence": 8,
-        "scf_type": "df",
-    },
-)
-
-spec_mb = ManybodySpecification(
-    program='qcmanybody',
-    bsse_correction=['cp', 'nocp'],
-    levels={
-        1: qc_spec_mb,
-        2: qc_spec_mb,
-    },
-)
-print("spec_mb", spec_mb)
-
-ds_mb.add_specification(name=f"psi4/{method}/{basis}", specification=spec_mb)
-
-# Run the computations
-ds_mb.submit()
-print(f"Submitted {ds_name} dataset")
-
-# Check the status of the dataset - can repeatedly run this to see the progress
-ds_mb.status()
+# Can delete the dataset if you want to start over. Need to know dataset_id
+# client.delete_dataset(dataset_id=2, delete_records=True)
 
 # |%%--%%| <g31JlHrgso|bYERcUudd0>
 
@@ -274,10 +265,12 @@ ds_mb.status()
 
 # |%%--%%| <bYERcUudd0|gauw3VIjl9>
 
-# Want multiple levels of theory
+# Set multiple levels of theory - you can add/remove levels as you desire.
+# Computational scaling will get quite expensive with better methods and larger
+# basis sets
 
 methods = [
-    'hf', 'pbe', 'b3lyp',
+    'hf', 'pbe',
 ]
 basis_sets = [
     '6-31g*'
@@ -325,12 +318,11 @@ pp(ds_mb.status())
 pp(ds)
 pp(ds_mb)
 pp(ds_mb.computed_properties)
-# client.delete_dataset(dataset_id=2, delete_records=True)
 
 # |%%--%%| <ChCOdcBiXj|7RHL31QOoC>
 
-# Multipole Molecule assemble
-def assemble_multipole_data(record):
+# Singlepoint data assemble
+def assemble_singlepoint_data(record):
     record_dict = record.dict()
     qcvars = record_dict["properties"]
     level_of_theory = f"{record_dict['specification']['method']}/{record_dict['specification']['basis']}"
@@ -368,7 +360,6 @@ def assemble_multipole_data(record):
         sapt_energies[2] = qcvars['sapt exch energy']
         sapt_energies[3] = qcvars['sapt ind energy']
         sapt_energies[4] = qcvars['sapt disp energy']
-        # pp(record_dict)
         return (
         record.molecule,
         None,
@@ -387,7 +378,7 @@ def assemble_multipole_data(record):
 
 
 
-def assemble_multipole_data_value_names():
+def assemble_singlepoint_data_value_names():
     return [
         'qcel_molecule',
         "volume ratios",
@@ -404,21 +395,14 @@ def assemble_multipole_data_value_names():
     ]
 
 df = ds.compile_values(
-    value_call=assemble_multipole_data,
-    value_names=assemble_multipole_data_value_names(),
+    value_call=assemble_singlepoint_data,
+    value_names=assemble_singlepoint_data_value_names(),
     unpack=True,
 )
-# print(df)
 pp(df.columns.tolist())
-print(df['psi4/hf/sto-3g'])
-pp(df['psi4/hf/sto-3g'].columns.tolist())
 df_sapt0 = df['psi4/SAPT0/cc-pvdz']
-print(df_sapt0.columns.tolist())
-print(df_sapt0)
 
 # |%%--%%| <7RHL31QOoC|CDD1QjxHpc>
-
-h2kcalmol = qcel.constants.hartree2kcalmol
 
 def assemble_data(record):
     record_dict = record.dict()
@@ -453,25 +437,18 @@ df_mb = ds_mb.compile_values(
     unpack=True,
 )
 
-print(df_mb)
+pp(df_mb.columns.tolist())
 
 # |%%--%%| <CDD1QjxHpc|XT87RegBfm>
 
 from cdsg_plot import error_statistics
 
-pp(df_mb.columns.tolist())
-print(len(df_sapt0))
-print(len(df_sapt0['SAPT Energies']))
-print(len(df_sapt0['SAPT Energies'].apply(lambda x: x[0] * h2kcalmol)))
-
 df_sapt0['sapt0 total energes'] = df_sapt0['SAPT Energies'].apply(lambda x: x[0] * h2kcalmol)
-print(df_sapt0['sapt0 total energes'])
 df_plot = pd.DataFrame(
     {
-        "qcel_molecule": df_mb["psi4/hf/sto-3g"]["qcel_molecule"],
+        "qcel_molecule": df_mb["psi4/pbe/6-31g*"]["qcel_molecule"],
         "HF/6-31G*": df_mb["psi4/hf/6-31g*"]["CP_IE"],
         "PBE/6-31G*": df_mb["psi4/pbe/6-31g*"]["CP_IE"],
-        "B3LYP/6-31G*": df_mb["psi4/b3lyp/6-31g*"]["CP_IE"],
         'SAPT0/cc-pvdz': df_sapt0['sapt0 total energes'].values,
     }
 )
@@ -480,13 +457,14 @@ id = [int(i[7:]) for i in df_plot.index]
 df_plot['id'] = id
 df_plot.sort_values(by='id', inplace=True, ascending=True)
 df_plot['reference'] = ref_IEs
+df_plot['SAPT0/aug-cc-pvdz'] = sapt0_adz
 df_plot['HF/6-31G* error'] = (df_plot['HF/6-31G*'] - df_plot['reference']).astype(float)
 df_plot['PBE/6-31G* error'] = (df_plot['PBE/6-31G*'] - df_plot['reference']).astype(float)
-df_plot['B3LYP/6-31G* error'] = (df_plot['B3LYP/6-31G*'] - df_plot['reference']).astype(float)
 df_plot['SAPT0/cc-pvdz error'] = (df_plot['SAPT0/cc-pvdz'] - df_plot['reference']).astype(float)
+df_plot['SAPT0/aug-cc-pvdz error'] = (df_plot['SAPT0/aug-cc-pvdz'] - df_plot['reference']).astype(float)
 pd.set_option('display.max_rows', None)
-print(df_plot)
-print(df_plot[['HF/6-31G* error', 'PBE/6-31G* error', 'B3LYP/6-31G* error']].describe())
+print(df_plot[['PBE/6-31G*', 'SAPT0/cc-pvdz', 'reference', "SAPT0/aug-cc-pvdz"]])
+print(df_plot[['PBE/6-31G* error', 'SAPT0/cc-pvdz error', "SAPT0/aug-cc-pvdz error"]].describe())
 
 # |%%--%%| <XT87RegBfm|dRiuyCtOh1>
 
@@ -495,11 +473,15 @@ error_statistics.violin_plot(
     df_labels_and_columns={
         "HF/6-31G*": "HF/6-31G* error",
         "PBE/6-31G*": "PBE/6-31G* error",
-        "B3LYP/6-31G*": "B3LYP/6-31G* error",
+        # "B3LYP/6-31G*": "B3LYP/6-31G* error",
         "SAPT0/cc-pvdz": "SAPT0/cc-pvdz error",
+        "SAPT0/aug-cc-pvdz": "SAPT0/aug-cc-pvdz error",
     },
     output_filename="S22-IE.png",
-    figure_size=(8, 6),
+    figure_size=(6, 6),
+    x_label_fontsize=16,
+    ylim=(-15, 15),
+    ylabel=r"IE Error vs. CCSD(T)/CBS ($\mathrm{kcal\cdot mol^{-1}}$)",
 )
 
 # |%%--%%| <dRiuyCtOh1|8jtfD3m3S4>
@@ -507,9 +489,12 @@ error_statistics.violin_plot(
 import apnet_pt
 from apnet_pt.AtomPairwiseModels.apnet2 import APNet2Model
 
-ap2 = APNet2Model().set_pretrained_model(model_id=0)
+atom_model = apnet_pt.AtomModels.ap2_atom_model.AtomModel().set_pretrained_model(model_id=0)
+ap2 = APNet2Model(atom_model=atom_model.model).set_pretrained_model(model_id=0)
+ap2.atom_model = atom_model.model
 apnet2_ies_predicted = ap2.predict_qcel_mols(
     mols=df_plot['qcel_molecule'].tolist(),
+    batch_size=16
 )
 
 # |%%--%%| <8jtfD3m3S4|nxmLPrfAfx>
@@ -517,37 +502,53 @@ apnet2_ies_predicted = ap2.predict_qcel_mols(
 # AP-Net2 IE
 df_plot['APNet2'] = np.sum(apnet2_ies_predicted, axis=1)
 df_plot['APNet2 error'] = (df_plot['APNet2'] - df_plot['reference']).astype(float)
-print(df_plot[['APNet2', 'reference', 'PBE/6-31G*']])
-print(apnet2_ies_predicted)
+print(df_plot.sort_values(by='APNet2 error', ascending=True)[['APNet2', 'reference']])
 error_statistics.violin_plot(
     df_plot,
     df_labels_and_columns={
         "HF/6-31G*": "HF/6-31G* error",
         "PBE/6-31G*": "PBE/6-31G* error",
-        "B3LYP/6-31G*": "B3LYP/6-31G* error",
         "SAPT0/cc-pvdz": "SAPT0/cc-pvdz error",
+        "SAPT0/aug-cc-pvdz": "SAPT0/aug-cc-pvdz error",
         "APNet2": "APNet2 error",
     },
     output_filename="S22-IE-AP2.png",
     figure_size=(4, 4),
 )
 
-# |%%--%%| <nxmLPrfAfx|XtMJQEokjd>
+# |%%--%%| <nxmLPrfAfx|ROLVxSHnj2>
 
-# Training models on new QM data for Transfer Learning Task
+from apnet_pt.pt_datasets.dapnet_ds import dapnet2_module_dataset_apnetStored
+
+# ds = dapnet2_module_dataset_apnetStored(
+#     root=data_path,
+#     r_cut=5.0,
+#     r_cut_im=8.0,
+#     spec_type=8,
+#     max_size=None,
+#     force_reprocess=True,
+#     atom_model_path=am_path,
+#     batch_size=batch_size,
+#     datapoint_storage_n_objects=datapoint_storage_n_objects,
+#     prebatched=prebatched,
+#     num_devices=1,
+#     skip_processed=False,
+#     skip_compile=True,
+#     print_level=2,
+#     m1="Elst_aug", 
+#     m2="Exch_aug",
+# )
+
+# |%%--%%| <ROLVxSHnj2|XtMJQEokjd>
+
+# Training models on new QM data: Transfer Learning
 
 from apnet_pt import pairwise_datasets
-
-print(
-    df_plot['qcel_molecule'].tolist(),
-    df_plot['PBE/6-31G*'].tolist(),
-)
-print(len(df_plot['qcel_molecule'].tolist()))
 
 ds2 = pairwise_datasets.apnet2_module_dataset(
     root="data_dir",
     spec_type=None,
-    atom_model=apnet_pt.AtomModels.ap2_atom_model.AtomModel().set_pretrained_model(model_id=0),
+    atom_model=atom_model,
     qcel_molecules=df_plot['qcel_molecule'].tolist(),
     energy_labels=[np.array([i]) for i in df_plot['reference'].tolist()],
     skip_compile=True,
@@ -555,99 +556,50 @@ ds2 = pairwise_datasets.apnet2_module_dataset(
     atomic_batch_size=8,
     prebatched=False,
     in_memory=True,
-    batch_size=2,
-    # datapoint_storage_n_objects=4,
+    batch_size=4,
 )
-print("APNet2 dataset created")
 print(ds2)
-print(ds2.training_batch_size)
-# print(ds2.data)
 
 # |%%--%%| <XtMJQEokjd|xXslqNQSRI>
 
 # Transfer Learning APNet2 model on computed QM data
-print(ds2)
 ap2.train(
     dataset=ds2,
-    n_epochs=10,
+    n_epochs=50,
     transfer_learning=True,
     skip_compile=True,
     model_path="apnet2_transfer_learning.pt",
     split_percent=0.8,
 )
-print(ap2)
 
 # |%%--%%| <xXslqNQSRI|KPOeFMBhWm>
-
-ap2_best_test = APNet2Model(
-    atom_model=ap2.atom_model,
-    pre_trained_model_path="apnet2_transfer_learning.pt",
-)
-print(ap2_best_test)
 
 # AP-Net2 IE
 apnet2_ies_predicted_final = ap2.predict_qcel_mols(
     mols=df_plot['qcel_molecule'].tolist(),
-    batch_size=2,
+    batch_size=16,
 )
-apnet2_ies_predicted_best_test = ap2_best_test.predict_qcel_mols(
-    mols=df_plot['qcel_molecule'].tolist(),
-    batch_size=2,
-)
-print(apnet2_ies_predicted_final)
-print(apnet2_ies_predicted_best_test)
 df_plot['APNet2 final'] = np.sum(apnet2_ies_predicted_final, axis=1)
-df_plot['APNet2 best test'] = np.sum(apnet2_ies_predicted_best_test, axis=1)
-print(df_plot[['APNet2 final', 'APNet2 best test', 'reference', 'PBE/6-31G*']])
 df_plot['APNet2 final error'] = (df_plot['APNet2 final'] - df_plot['reference']).astype(float)
-df_plot['APNet2 best test error'] = (df_plot['APNet2 best test'] - df_plot['reference']).astype(float)
 
 error_statistics.violin_plot(
     df_plot,
     df_labels_and_columns={
         "HF/6-31G*": "HF/6-31G* error",
         "PBE/6-31G*": "PBE/6-31G* error",
-        "B3LYP/6-31G*": "B3LYP/6-31G* error",
+        "SAPT0/aug-cc-pvdz": "SAPT0/aug-cc-pvdz error",
         "APNet2": "APNet2 error",
         "APNet2 final": "APNet2 final error",
-        "APNet2 best test": "APNet2 best test error",
     },
     output_filename="S22-IE-AP2.png",
-    figure_size=(4, 4),
+    figure_size=(6, 4),
 )
 
 # |%%--%%| <KPOeFMBhWm|OVVcYRXWUA>
 
 # Be careful with this for it can corrupt running status...
-!ps aux | grep qcfractal | awk '{ print $2 }' | xargs kill -9
+# !ps aux | grep qcfractal | awk '{ print $2 }' | xargs kill -9
 
 # |%%--%%| <OVVcYRXWUA|ULuCE0r8ED>
-
-# Load in a dataset from a recent Sherrill work (Levels of SAPT II)
-df_LoS = pd.read_pickle("./combined_df_subset_358.pkl")
-print(df_LoS[['Benchmark', 'SAPT2+3(CCD)DMP2 TOTAL ENERGY aqz', 'MP2 IE atz', 'SAPT0 TOTAL ENERGY adz' ]])
-
-# Limit to 100 molecules with maximum of 16 atoms to keep computational cost down
-df_LoS['size'] = df_LoS['atomic_numbers'].apply(lambda x: len(x))
-df_LoS = df_LoS[df_LoS['size'] <= 16]
-df_LoS = df_LoS.sample(100, random_state=42, axis=0).copy()
-df_LoS.reset_index(drop=True, inplace=True)
-print(df_LoS['size'].describe())
-
-# Create QCElemntal Molecules to generate the dataset
-def qcel_mols(row):
-    """
-    Convert the row to a qcel molecule
-    """
-    atomic_numbers = [row['atomic_numbers'][row['monAs']], row['atomic_numbers'][row['monBs']]]
-    coords = [row['coordinates'][row['monAs']], row['coordinates'][row['monBs']]]
-    cm = [
-        [row['monA_charge'], row['monA_multiplicity']],
-        [row['monB_charge'], row['monB_multiplicity']],
-     ]
-    return tools.convert_pos_carts_to_mol(atomic_numbers, coords, cm)
-df_LoS['qcel_molecule'] = df_LoS.apply(qcel_mols, axis=1)
-
-# |%%--%%| <ULuCE0r8ED|5HSrFMKRJh>
 
 
